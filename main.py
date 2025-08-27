624d0a87603dcec58615b997ed68ee8f32d11342
Dron <mister228orange@gmail.com>
2025-08-28 04:40:43 +0500

    Extend readme

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def square_equation_solve(a, b, c):
+    '''
+    Решаем уравнение вида a*x^2 + bx + c = 0
+    '''
